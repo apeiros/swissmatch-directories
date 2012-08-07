73484cdfe9a343fceb0580e3f3dda4476f6cb6c7
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-08-07 14:29:48 +0200

    Added a proper hub file.

---
+++ lib/swissmatch/directories.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+
+
+require 'swissmatch/directoryservice'
+require 'swissmatch/telsearch'
+require 'swissmatch/address'
+
+
+
+# From SwissMatch::Directories
+# Query address data from swiss directory providers.
+#
+# @note
+#   All strings passed to SwissMatch are expected to be utf-8. All strings
+#   returned by SwissMatch are also in utf-8.
+#
+module SwissMatch
+  @directory_service  = nil
+
+  class <<self
+    # @return [SwissMatch::DirectoryService, nil]
+    # The directory service used to search for addresses
+    attr_accessor :directory_service
+  end
+end
